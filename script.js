cdf11fa3bb6206d64dc6b1fdbcbc2d499299568e
Daniel Burgoa <danielburgoa50@gmail.com>
2024-04-27 00:06:30 -0500

    Project Finished

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Function to generate random passwords
 // NOTE: Default password lenght is 8, default use of symbols is true
 
-const createRandomPass = (length = 8, useSymbols = true) => {
+const createRandomPass = (length = 8, useSymbols = false) => {
   const uppercase = [
     "A",
     "B",
@@ -59,7 +59,71 @@ const createRandomPass = (length = 8, useSymbols = true) => {
   return output;
 };
 
-// DOM Manipulation:
-const passwordLengthInput = document.querySelectorAll(".length-input");
+// Track if the user want symbols or not
+let symbols = false;
 
-console.log(passwordLengthInput.value);
+// Display password or error messages
+function displayPassword() {
+  const length = +passwordLengthInput.value;
+
+  if (length < 4 || length > 20) {
+    msgOutput.textContent =
+      "Please choose a length between 4 and 20 characters.";
+    msgOutput.classList.remove("hidden");
+    msgOutput.classList.remove("success");
+    passwordLengthInput.value = "4";
+    passwordOutput.textContent = "Awaiting for Password...";
+  } else {
+    passwordOutput.textContent = createRandomPass(length, symbols);
+    msgOutput.classList.add("hidden");
+  }
+}
+
+// Copy password to clipboard
+const copyContent = async () => {
+  try {
+    await navigator.clipboard.writeText(passwordOutput.textContent);
+    msgOutput.classList.remove("hidden");
+    msgOutput.classList.add("success");
+    msgOutput.textContent = "Your password was copied to the clipboard!";
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
+
+// DOM Manipulation:
+const passwordLengthInput = document.querySelector(".length-input");
+const createPasswordButton = document.querySelector(".generate-password");
+const msgOutput = document.querySelector(".message");
+const passwordOutput = document.querySelector(".password-output");
+const symbolsInput = document.querySelector(".symbol-input");
+const symbolsIcon = document.querySelector(".symbols-icon");
+const copyBtn = document.querySelector(".copy-icon");
+
+passwordLengthInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    displayPassword();
+    e.preventDefault();
+  }
+});
+
+createPasswordButton.addEventListener("click", (e) => {
+  displayPassword();
+});
+
+symbolsInput.addEventListener("click", () => {
+  symbols = !symbols;
+  console.log(symbols);
+  symbolsIcon.classList.toggle("hidden");
+});
+
+// Copy password to clipboard
+
+copyBtn.addEventListener("click", () => {
+  if (passwordOutput.textContent === "Awaiting for Password...") {
+    msgOutput.textContent = "Please generate a valid password";
+    msgOutput.classList.remove("hidden");
+  } else {
+    copyContent();
+  }
+});
